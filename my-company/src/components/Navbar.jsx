64e45050acc4239bcd40ac8942a20b092bf9985a
Abubakar Sadiq Ibrahim <iabu6133@gmail.com>
2026-01-24 13:03:15 +0000

    Created a 4 page website

---
+++ my-company/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+    return (
+        <nav style={{ padding: '10px', backgroundColor: '#333', color: '#fff' }}>
+            <Link to="/" style={{ color: '#fff', marginRight: '15px', textDecoration: 'none' }}>Home</Link>
+            <Link to="/about" style={{ color: '#fff', marginRight: '15px', textDecoration: 'none' }}>About</Link>
+            <Link to="/services" style={{ color: '#fff', marginRight: '15px', textDecoration: 'none' }}>Services</Link>
+            <Link to="/contact" style={{ color: '#fff', textDecoration: 'none' }}>Contact</Link>
+        </nav>
+    );
+}
+
+export default Navbar;
