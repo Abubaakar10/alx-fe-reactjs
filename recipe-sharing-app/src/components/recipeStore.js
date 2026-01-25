1e4b8a94d899c079ad5b08171f61e080b436f59a
Abubakar Sadiq Ibrahim <iabu6133@gmail.com>
2026-01-25 10:59:01 +0000

    Fixed importation of files from wrong directories

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,12 +2,10 @@ import { create } from 'zustand';
 
 const useRecipeStore = create((set) => ({
     recipes: [],
-
     addRecipe: (newRecipe) =>
         set((state) => ({
             recipes: [...state.recipes, newRecipe],
         })),
-
     setRecipes: (recipes) => set({ recipes }),
 }));
 
