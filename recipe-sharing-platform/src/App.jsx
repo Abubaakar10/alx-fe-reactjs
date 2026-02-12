234eb7f27150e4fb9570cabed5869b39a56487ca
Abubakar Sadiq Ibrahim <iabu6133@gmail.com>
2026-02-12 13:06:01 +0000

    chore: initialize new React + Tailwind project with UserProfile component

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50">
+      <h1 className="text-3xl font-bold text-blue-500">
+        Tailwind is working!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
+
