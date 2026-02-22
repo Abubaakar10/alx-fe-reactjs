17d840bd4b9ebb0b60434ba109871d72a1d5594d
Abubakar Sadiq Ibrahim <iabu6133@gmail.com>
2026-02-22 15:40:52 +0000

    replaced controlled form with Formik Registration

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,11 +1,8 @@
-import RegistrationForm from "./components/RegistrationForm";
 import FormikForm from "./components/FormikForm";
 
 function App() {
   return (
     <div>
-      <RegistrationForm />
-      <hr />
       <FormikForm />
     </div>
   );
